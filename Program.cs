70583c6e547d769c48615c56f85a8195433e0527
Richi1146 <ricardo225.x@gmail.com>
2025-12-02 20:06:48 -0500

    feat(api): enable swagger in all environments

--- Program.cs
+++ Program.cs
@@ -32,11 +32,8 @@ builder.Services.AddDbContext<ParadoxContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
